483a963635d66f558f2a3a8edca7661f2a52dda5
Mats Lindh <mats@lindh.no>
2015-07-30 20:49:23 +0200

    Add dummy views and configure routes

--- kimochi/__init__.py
+++ kimochi/__init__.py
@@ -17,6 +17,7 @@ def main(global_config, **settings):
     config.include('pyramid_beaker')
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.add_route('index', '/')
+    config.add_route('login', '/login')
     config.scan()
     return config.make_wsgi_app()
