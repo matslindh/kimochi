101353065d07d16dc094c3aa4d04ee1472d50e05
Mats Lindh <mats@lindh.no>
2015-09-13 22:44:05 +0200

    API: Implement page view and api_key verification

--- kimochi/api.py
+++ kimochi/api.py
@@ -4,20 +4,46 @@ from pyramid.view import (
     forbidden_view_config,
 )
 
+from pyramid.httpexceptions import (
+    HTTPUnauthorized,
+    HTTPNotFound,
+)
+
 from .models import (
     Site,
+    Page,
+    NotFoundException,
+    NoAccessException,
 )
 
 @view_defaults(renderer='json', permission='api')
-class Site:
+class SiteAPI:
     def __init__(self, request):
         self.request = request
-        self.site = Site.get_from_key(request.matchdict['site_key'])
-        print("- --- - - - - - - - -- - - - - - WE ARE INIT")
+
+        if 'api_key' not in request.GET:
+            raise HTTPUnauthorized
+
+        try:
+            site = Site.get_from_key_and_api_key(request.matchdict['site_key'], request.GET.getone('api_key'))
+        except NotFoundException:
+            raise HTTPNotFound
+        except NoAccessException:
+            raise HTTPUnauthorized
+
+        self.site = site
 
     @view_config(route_name='api_site_page')
     def page(self):
-        return {'page': 'beep boop'}
+        page = Page.get_for_site_id_and_page_id(self.site.id, self.request.matchdict['page_id'])
+
+        if not page or not page.published:
+            raise HTTPNotFound
+
+        return {
+            'site': self.site,
+            'page': page,
+        }
 
     @view_config(route_name='api_site_gallery')
     def gallery(self):
