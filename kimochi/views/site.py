57a5904af336a17d8f42c72f31591253b73df981
Mats Lindh <mats@lindh.no>
2015-09-17 20:44:14 +0200

    Move site update handling to its own view predicate

--- kimochi/views/site.py
+++ kimochi/views/site.py
@@ -36,8 +36,8 @@ def site_add(request):
 
     return HTTPSeeOther(location=request.route_url('site', site_key=site.key))
 
-@view_config(route_name='site', renderer='kimochi:templates/site.mako')
-def site(request):
+@view_config(route_name='site', request_method='POST', renderer='kimochi:templates/site.mako')
+def site_update(request):
     site = Site.get_from_key_and_user_id(request.matchdict['site_key'], authenticated_userid(request))
 
     if 'command' in request.POST:
@@ -55,18 +55,18 @@ def site(request):
 
         request.session.flash("Footer text has been updated!")
 
-        return HTTPSeeOther(
-            location=request.current_route_url()
-        )
-
     if 'select_index_page' in request.POST and site.get_active_page(request.POST['select_index_page']):
         site.set_default_index_page(site.get_active_page(request.POST['select_index_page']))
 
         request.session.flash("The default index page has been updated.")
 
-        return HTTPSeeOther(
-            location=request.current_route_url()
-        )
+    return HTTPSeeOther(
+        location=request.current_route_url()
+    )
+
+@view_config(route_name='site', renderer='kimochi:templates/site.mako')
+def site(request):
+    site = Site.get_from_key_and_user_id(request.matchdict['site_key'], authenticated_userid(request))
 
     return {
         'site': site,
